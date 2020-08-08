0fed081320f35c63eac99d84b17bab86a50caa1f
Luke Olson <luke.olson@gmail.com>
2020-08-08 08:06:38 -0500

    add readme

--- bench1.py
+++ bench1.py
@@ -1,29 +1,62 @@
-import numpy as np
-from time import time
+"""bench1, time a file write of a numpy array."""
+
 import os
+from time import time
+import numpy as np
 
-nlist = [1024*128*int(k) for k in np.logspace(1,4,20)] # sizes, in MB
-ntests = 5
 
-tlist = np.zeros((ntests, len(nlist)))
-mblist = np.zeros(len(nlist))
+def bench(nlist, ntests=5, outname='bench1.npz'):
+    """
+    Parameters
+    ----------
+    nlist : array-like
+        List if sizes to tests in MB
+    ntests : int
+        Number of tests to run
 
-for j, n in enumerate(nlist):
-    print(f'{j} of {len(nlist)}')
-    a = np.random.rand(n)
+    Return
+    ------
+    tlist : ndarray
+        List of times, ntests x len(nlist)
+    mblist : ndarray
+        List of MB
+    outname : string
+        If None, tlist and mblist are returned.
+        Else, tlist, mblist are also written to file `outname` or `outname.npz`
+    """
 
-    # open / write
-    for i in range(ntests):
-        tstart = time()
-        fname = f'test-{n}-{i}.bin'
-        with open(fname, 'wb') as f:
-            a.tofile(f)
-        tend = time()
-        tlist[i, j] = tend - tstart
+    tlist = np.zeros((ntests, len(nlist)))
+    mblist = np.zeros(len(nlist))
 
-        if os.path.exists(fname):
-          os.remove(fname)
+    for j, n in enumerate(nlist):
+        # set the array
+        a = np.random.rand(n)
+        mb = a.nbytes / 1024 / 1024
 
-    mblist[j] = a.nbytes / 1024 / 1024
+        print(f'test {j+1} of {len(nlist)} ({mb:.1f} MB)')
+        mblist[j] = mb
 
-np.savez('bench1.npz', tlist=tlist, mblist=mblist)
+        # open / write
+        for i in range(ntests):
+            fname = f'test-{n}-{i}.bin'
+
+            tstart = time()
+            with open(fname, 'wb') as f:
+                a.tofile(f)
+            tend = time()
+            tlist[i, j] = tend - tstart
+
+            if os.path.exists(fname):
+                os.remove(fname)
+
+    if outname is not None:
+        np.savez(outname, tlist=tlist, mblist=mblist)
+
+    return tlist, mblist
+
+
+if __name__ == '__main__':
+    # list of sizes in MB
+    nlist = [1024 * 128 * int(k) for k in np.logspace(1, 2, 8)]
+    ntests = 5
+    bench(nlist, ntests, 'bench1-test.npz')
---
+++ readme.md
@@ -0,0 +1,3 @@
+# a simple benchmark
+
+- `bench1`: tests the write speed of a numpy array
